70ddb8be20ee38ec4fb5c86c048d81bb4e3f5aaa
Abel Toth <abeltoth7@gmail.com>
2022-02-22 23:06:30 +0100

    Animation loop added

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,4 +1,4 @@
-import { BoxBufferGeometry, Mesh, MeshStandardMaterial } from 'https://cdn.skypack.dev/three';
+import { BoxBufferGeometry, Mesh, MeshStandardMaterial, MathUtils } from 'https://cdn.skypack.dev/three';
 
 function createCube() {
   // create a geometry
@@ -12,6 +12,16 @@ function createCube() {
   const cube = new Mesh(geometry, material);
 
   cube.rotation.set(-0.5, -0.1, 0.8);
+
+  const radiansPerSecond = MathUtils.degToRad(30);
+
+  // this method will be called once per frame
+  cube.tick = (delta) => {
+    // increase the cube's rotation each frame
+    cube.rotation.z += radiansPerSecond * delta;
+    cube.rotation.x += radiansPerSecond * delta;
+    cube.rotation.y += radiansPerSecond * delta;
+  };
 
   return cube;
 }
--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@ function main() {
   // 1. Create an instance of the World app
   const world = new World(container);
 
-  // 2. Render the scene
-  world.render();
+  // produce a single frame (render on demand)
+  // world.render();
+
+  // start the loop (produce a stream of frames)
+  world.start();
 }
 
 main();
